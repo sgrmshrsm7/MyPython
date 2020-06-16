c426ad2fbfee33c21280f523d4efe52779696adf
Sagar Mishra <sgrmshrsm7@gmail.com>
2020-06-16 20:04:54 +0530

    modified code

--- pe/smallestmultiple.py
+++ pe/smallestmultiple.py
@@ -1,15 +1,14 @@
-#2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-#What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-i=1
-num = 2520
-while i!=0:
-    num = num + 1
-    count = 0
-    for j in range(1,21):
-        if num % j == 0:
-            count = count + 1
-    if count == 20:
-        i=0
+primes = [2, 3, 5, 7, 11, 13, 17, 19]
 
-print (num)
+num = 1
+
+for i in primes:
+    p = i
+    while p * i < 20:
+        p *= i
+    num *= p
+
+print(num)

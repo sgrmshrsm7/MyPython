5efafa7c902459f6e632a0c9889086ab9af87fb1
Sagar Mishra <sgrmshrsm7@gmail.com>
2020-06-16 17:34:02 +0530

    modified according to python3

--- pe/amicablenumbers.py
+++ pe/amicablenumbers.py
@@ -3,7 +3,7 @@
 #   For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
 #   Evaluate the sum of all the amicable numbers under 10000.
 
-array = range(1,10000)
+array = list(range(1,10000))
 
 sum = 0
 

79de4f666e271b811ecb8e06dc712f9dff384c2e
Sagar Mishra <sgrmshrsm7@gmail.com>
2020-06-25 20:31:01 +0530

    truncatableprimes(modified).py

---
+++ pe/truncatableprimes(modified).py
@@ -0,0 +1,40 @@
+# The number 3797 has an interesting property. Being prime itself, it is possible to continuously remove digits from left to right, and remain prime at each stage: 3797, 797, 97, and 7. Similarly we can work from right to left: 3797, 379, 37, and 3.
+# Find the sum of the only eleven primes that are both truncatable from left to right and right to left.
+# NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
+
+sum = 0
+
+primes = []
+numbers = [0] * 1000000
+p = 2
+
+numbers[0] = 1
+numbers[1] = 1
+
+while p < 1000000:
+        if numbers[p] == 0:
+                primes.append(p)
+                i = 2 * p
+                while i < 1000000:
+                        numbers[i] = 1
+                        i += p
+        p += 1
+
+i = 0
+
+while i < 11:
+        for j in primes[8:]:
+                j1 = str(j)
+                kk = 1
+                f = 1
+                while kk < len(j1) and f:
+                        if numbers[int(j1[kk:])] == 1 or numbers[int(j1[:kk])] == 1:
+                                f = 0
+                        kk += 1
+                if f:
+                        sum += j
+                        i += 1
+                if i == 11:
+                        break
+
+print(sum)
